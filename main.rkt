080f005c1821d7359f44351ca984d7eaa2934c10
William Berman <wlbberman@gmail.com>
2020-04-17 20:07:20 -0700

    move reader to main

--- main.rkt
+++ main.rkt
@@ -1,6 +1,15 @@
-#lang reader "reader.rkt"
-4
-2
-3
+#lang racket
 
+(require "datum.rkt")
 
+(provide (rename-out [x-read-syntax read-syntax])
+         (rename-out [x-read read]))
+
+(define (x-read-syntax path port)
+  (define the-arguments (format-datums '~a (port->lines port)))
+  (define the-module `(module stacker-module "expander.rkt" (stack-program ,@the-arguments)))
+  (define the-syntax (datum->syntax #f the-module))
+  the-syntax)
+
+(define (x-read in)
+  (x-read-syntax #f in))
